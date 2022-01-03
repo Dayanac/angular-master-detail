e0ff7895d68c231713ac90e3c6e742f0455da33a
Dayana Oliveira <dayanacoliveira@hotmail.com>
2022-01-03 17:18:07 -0300

    Criando componentes de errors

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { RouterModule } from '@angular/router';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
 
 @NgModule({
   declarations: [
-    BreadCrumbComponent
+    BreadCrumbComponent,
+    PageHeaderComponent,
+    FormFieldErrorComponent
   ],
   imports: [
     CommonModule,
@@ -16,7 +20,9 @@ import { RouterModule } from '@angular/router';
   exports: [
     CommonModule,
     ReactiveFormsModule,
+    PageHeaderComponent,
     BreadCrumbComponent,
+    FormFieldErrorComponent,
     RouterModule
   ]
 })
