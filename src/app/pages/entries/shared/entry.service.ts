067a7d4fc13aacab273e0d3acf97571dac3520bf
Dayana Oliveira <dayanacoliveira@hotmail.com>
2022-01-06 15:37:17 -0300

    Aprimorando reports

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -6,7 +6,9 @@ import { Entry } from './entry.model';
 
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 import { Observable } from 'rxjs';
-import { catchError, flatMap } from 'rxjs/operators';
+import { catchError, flatMap, map } from 'rxjs/operators';
+
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -28,6 +30,12 @@ export class EntryService extends BaseResourceService<Entry>{
     return this.setCategoryAndSendToServer(entry, super.update.bind(this));
   }
 
+  getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthYear(entries, month, year))
+    )
+  }
+
   private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
@@ -36,5 +44,15 @@ export class EntryService extends BaseResourceService<Entry>{
       }),
       catchError(this.handleError)
     )
+  }
+
+  private filterByMonthYear(entries: Entry[], month: number, year: number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, "DD/MM/YYYY");
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+
+      if(monthMatches && yearMatches) return entry;
+    })
   }
 }
